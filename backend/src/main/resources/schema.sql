d27309ea3cb8b5a6412d9518bb124d2ca99a7206
Razi <syedru@mcmaster.ca>
2024-07-15 12:32:46 -0400

    use MySQL instead of H2

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS todo (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title varchar(255) NOT NULL,
-    description CLOB,
+    description TEXT,
     completed BOOLEAN NOT NULL
 );
